3e4900588401bf6b8af8b44b18fa5acfe82a2777
Jether Netto <jether@new-host-18.home>
2014-06-05 18:57:20 -0300

    First Commit

---
+++ TSwift/TSwift/MathSum.h
@@ -0,0 +1,15 @@
+//
+//  MathSum.h
+//  TSwift
+//
+//  Created by Jether on 14-6-5.
+//  Copyright (c) 2014 Jether. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MathSum : NSObject
+
+- (NSInteger)sum:(NSInteger)number1 number2:(NSInteger)number2;
+
+@end
---
+++ TSwift/TSwift/MathSum.m
@@ -0,0 +1,19 @@
+//
+//  MathSum.m
+//  TSwift
+//
+//  Created by Jether on 14-6-5.
+//  Copyright (c) 2014 Jether. All rights reserved.
+//
+
+#import "MathSum.h"
+
+@implementation MathSum
+
+- (NSInteger)sum:(NSInteger)number1 number2:(NSInteger)number2
+{
+    return number1 + number2;
+}
+
+@end
+
